9f0bf439adbc59d2771458df3b1403f653d8cbe8
steven31518 <107013204+steven31518@users.noreply.github.com>
2024-03-04 15:03:36 +0800

    Update SocialButtonGroup.tsx

--- components/SocialButtonGroup.tsx
+++ components/SocialButtonGroup.tsx
@@ -1,10 +1,10 @@
 "use client";
+
 import React from "react";
 import { signIn } from "next-auth/react";
 import { Button } from "./ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { IoLogoDiscord } from "react-icons/io5";
-import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Separator } from "@/components/ui/separator";
 
